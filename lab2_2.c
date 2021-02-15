45c8d4a865aeb680de668e65550c8262674a9f1b
b6320500590 <nawapol.d@ku.th>
2021-02-15 20:31:18 +0700

    1

---
+++ lab2_2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    char promotion;
+    float time;
+
+    scanf("%c", &promotion);
+    fflush(stdin);
+    scanf("%f", &time);
+
+    if(promotion=='A')
+    {
+        if(time>200)
+            printf("%.2f", 199+(((int)time-200)*3)+(((time-(int)time)/0.6)*3));
+        else
+            printf("199.00");
+    }
+    else
+    {
+        if(time>400)
+            printf("%.2f", 299+(((int)time-400)*2)+(((time-(int)time)/0.6)*2));
+        else
+            printf("299.00");
+    }
+
+    return 0;
+}
